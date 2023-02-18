e4bfcd6497c68240af70a70c0afc5e3410a7b428
Douglas Natan <dnsg2006@gmail.com>
2023-02-18 16:42:05 -0300

    starting project

---
+++ BKP/20230131232757_indentityDB1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalPilot.Migrations
+{
+    public partial class indentityDB1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ BKP/20230131235324_IndentityContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MinimalPilot.Migrations
+{
+    public partial class IndentityContext : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
